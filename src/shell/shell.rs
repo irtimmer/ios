0a6e03cd9cf0a6a7c96e45cb8185f71b5e769e6e
Iwan Timmer <irtimmer@gmail.com>
2023-10-29 12:36:23 +0100

    shell: add ls and cat commands

--- src/shell/shell.rs
+++ src/shell/shell.rs
@@ -1,7 +1,9 @@
 use alloc::string::String;
+use alloc::sync::Arc;
 use alloc::vec::Vec;
 
 use core::fmt::Write;
+use core::future;
 
 use futures_util::StreamExt;
 
@@ -9,6 +11,8 @@ use crate::block::Block;
 use crate::block::mbr::Mbr;
 use crate::drivers::block::virtio_blk::VirtioBlk;
 use crate::drivers::i8042::KeyboardStream;
+use crate::fs::FileSystem;
+use crate::fs::vfat::VFat16;
 use crate::runtime::runtime;
 
 async fn read_line(kbd: &mut KeyboardStream<'_>) -> String {
@@ -35,6 +39,8 @@ pub async fn ios_shell() {
             "echo" => writeln!(runtime().console.lock(), "{}", args).unwrap(),
             "read" => read(args).await,
             "part" => part().await,
+            "ls" => ls().await,
+            "cat" => cat(args).await,
             _ => writeln!(runtime().console.lock(), "Command '{}' not found", cmd).unwrap(),
         }
     }
@@ -57,4 +63,34 @@ pub async fn part() {
     mbr.partitions().iter().enumerate().for_each({
         |(i, p)| writeln!(runtime().console.lock(), "Partition {}: {:?}", i, p).unwrap()
     })
+}
+
+pub async fn ls() {
+    let block = runtime().get::<VirtioBlk>().unwrap();
+
+    let mbr = Mbr::new(block).await.unwrap();
+    let partition = mbr.get_partition(0).await.unwrap();
+
+    let fs = VFat16::new(Arc::new(partition)).await.unwrap();
+    let mut entries = fs.listdir("/").await.unwrap();
+
+    while let Some(entry) = entries.next().await {
+        writeln!(runtime().console.lock(), "{:?}", entry).unwrap();
+    }
+}
+
+pub async fn cat(args: &str) {
+    let block = runtime().get::<VirtioBlk>().unwrap();
+
+    let mbr = Mbr::new(block).await.unwrap();
+    let partition = mbr.get_partition(0).await.unwrap();
+
+    let fs = Arc::new(VFat16::new(Arc::new(partition)).await.unwrap());
+    let entries = fs.listdir("/").await.unwrap();
+    let entry = entries.filter(|e| future::ready(e.name == args)).next().await.unwrap();
+
+    let mut file = fs.open_node(entry.inode, entry.length as u64).await.unwrap();
+    let mut buf = [0u8; 512];
+    let len = file.read(&mut buf).await.unwrap();
+    runtime().console.lock().write_str(&String::from_utf8_lossy(&buf[0..len])).unwrap();
 }
